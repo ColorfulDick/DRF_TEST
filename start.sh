e6d15109a4a927771b6683a32d3e38f6343f673c
zhongtong <1123063417@qq.com>
2021-06-19 06:47:12 +0000

    在docker中必须以前台进程来运行gunicorn，不然docker会认为容器已经挂掉

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 
 python3 manage.py collectstatic --noinput &&
 python3 manage.py migrate &&
-gunicorn Mydrf.asgi:application -D -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
+#gunicorn Mydrf.asgi:application -D -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 以后台进程运行gunicorn(加入-D参数)
+gunicorn Mydrf.asgi:application -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 #在docker中必须以前台进程来运行gunicorn，不然docker会认为容器已经挂掉
 
 #在源文件更改，镜像内的也会随之改变
